c83e1061e18ab9ecdc9979b702fdd202afe4a2d8
Lucas Montagnani <lucascalilelias@yahoo.com.br>
2025-04-21 18:48:19 -0300

    Add project files.

---
+++ Movies.API/Program.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Movies.API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+
+// Register In-Memory database context
+builder.Services.AddDbContext<MoviesAPIContext>(options =>
+                    options.UseInMemoryDatabase("MoviesDb"));
+
+// Register Authentication service with Bearer Token
+builder.Services.AddAuthentication("Bearer")
+                    // Configure support to the Authorization Server
+                    .AddJwtBearer("Bearer", options =>  
+                    {
+                        options.Authority = "https://localhost:5005"; // Address of the IdentityServer (when sending OpenId Calls)
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateAudience = false
+                        };
+                    });
+
+// Adding a Policy (restriction) specifying which values the specifed claim (first value) are allowed to have
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ClientIdPolicy", policy => policy.RequireClaim("client_id", "movieClient", "movies_mvc_client"));
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<MoviesAPIContext>();
+    // Make sure database is created (if not using migrations)
+    context.Database.EnsureCreated();
+    // Seed the database
+    MoviesContextSeed.SeedAsync(context);
+}
+
+app.UseHttpsRedirection();
+
+// Add Authentication Middleware to the pipeline
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,68 @@
+﻿using Movies.Client.Models;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {
+        //private readonly IHttpClientFactory _httpClientFactory;
+        //private readonly IHttpContextAccessor _httpContextAccessor;
+
+        //public MovieApiService(IHttpClientFactory httpClientFactory, IHttpContextAccessor httpContextAccessor)
+        //{
+        //    _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+        //    _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+        //}
+
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            var movies = new List<Movie>();
+
+            movies.Add(
+                    new Movie
+                    {
+                        Id = 1,
+                        Genre = "Drama",
+                        Title = "The Shawshank Redemption",
+                        Rating = "9.3",
+                        ImageUrl = "images/src",
+                        ReleaseDate = new DateTime(1994, 5, 5),
+                        Owner = "alice"
+                    }    
+                );
+            movies.Add(
+                    new Movie
+                    {
+                        Id = 2,
+                        Genre = "Crime",
+                        Title = "The Godfather",
+                        Rating = "9.2",
+                        ImageUrl = "images/src",
+                        ReleaseDate = new DateTime(1972, 5, 5),
+                        Owner = "alice"
+                    }
+                );
+
+            return await Task.FromResult(movies);
+        }
+
+        public Task<Movie> GetMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
